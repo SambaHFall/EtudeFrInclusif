0eb667e3b789bba850519d3f045d23e2a9542ff7
SambaHFall <hfallsamba@gmail.com>
2024-06-03 17:17:00 +0200

    Improved the method which load documents from oscar to create a dataset for inclusive french

--- collect_oscar.py
+++ collect_oscar.py
@@ -7,37 +7,38 @@ from src.adv_rule_based import *
 from src.super_model import SuperAnnModel
 import pandas as pd
 
+import os
+
 from datasets import load_dataset
 
 
 oscardataset = load_dataset("oscar-corpus/OSCAR-2201", use_auth_token=True, language="fr", streaming=True, split="train")
 
 def load_new_dataset_from_oscar() :
+	prog = 0
+    if os.path.exists('progress.txt'):
+        with open('progress.txt') as f:
+            prog = int(f.read().strip())
+        print(f'Skipping {prog} documents')
+        for i in tqdm(range(prog)):
+            next(oscardata)
+
 	modelSUPER = SuperAnnModel(models=[AdvRBModel(proc=['fle', 'coo']) , NaiveRBModel(), InclureModel()], weights=[1, 0.46, 1], tol=0.6)
-	cpt = 0
-	cur = 0
-	goal = 1000
-	maxiter = 20000
 
 	df = pd.DataFrame({"iddoc" : [], "context" : [], "range" : [], "text" : [], "category" : [] })
 
-	for d in oscardataset :
-		if len(d['text']) < 1000000 :
-			iddoc = d['id']
-			preds = modelSUPER.predict([d['text']])
-			if len(preds[0]) > 0 :
-				cpt += 1
-				df = pd.concat([df, modelSUPER.annotation_layout([d['text']], preds, iddoc=[iddoc]) ], ignore_index=True)
+	for i, d in enumerate(tqdm(oscardata)):
+		try :
+			if len(d['text']) < 1000000 :
+				iddoc = d['id']
+				preds = modelSUPER.predict([d['text']])
+				if len(preds[0]) > 0 :
+					df = pd.concat([df, modelSUPER.annotation_layout([d['text']], preds, iddoc=[iddoc]) ], ignore_index=True)
+		except KeyboardInterrupt :
+			with open('progress.txt', 'w') as f:
+                f.write(f'{i + prog}')
+            raise KeyboardInterrupt
 
-			if cpt < goal and cur < maxiter:
-				cur += 1
-				if cur % 20 == 0 :
-					with open("adv.text", 'w') as f :
-						f.write("nbiter=" + str(cur) + " / nbdocs=" + str(cpt) )
-					df.to_csv("dataset/SUPER_new_dataset.csv")
-			else :
-				df.to_csv("dataset/SUPER_new_dataset.csv")
-				break
 
 load_new_dataset_from_oscar()
 
