78453908e8b42499eb43946dea8f43076ac2a9da
SambaHFall <hfallsamba@gmail.com>
2024-06-20 10:39:49 +0200

    Changed README

--- _attic/example.py
+++ _attic/example.py
@@ -3,7 +3,6 @@ import os
 script_dir = os.path.dirname( os.path.abspath(__file__) )
 sys.path.append(os.path.dirname(script_dir))
 
-import spacy
 from etude_fr_inclusif.super_model import SuperAnnModel
 from etude_fr_inclusif.adv_rule_based import AdvRBModel
 from etude_fr_inclusif.inclure import InclureModel
